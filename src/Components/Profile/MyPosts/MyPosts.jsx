351ccfb8e246ff09bb0c6be2fd1ad2d20323ba8d
Sosisko <54500448+Sosisko@users.noreply.github.com>
2020-05-24 16:05:09 +0500

    Add files via upload

--- src/Components/Profile/MyPosts/MyPosts.jsx
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -7,6 +7,10 @@ const MyPosts = () => {
         <div>My posts
             <div>New post</div>
            <Post/>
+           <Post/>
+           <Post/>
+           <Post/>
+           <Post/>
         </div>
 
     );
--- src/Components/Profile/MyPosts/Post/Post.jsx
+++ src/Components/Profile/MyPosts/Post/Post.jsx
@@ -3,7 +3,13 @@ import s from './Post.module.css'
 
 const Post = () => {
     return (
-        <div>Post 1</div>
+        <div className={s.item}>
+            <img src="https://png.pngtree.com/element_origin_min_pic/16/12/28/7c566296b8f609148ff1b38c36e4aa79.jpg" alt=""/>
+            Post 1
+            <div>
+                <span>Like 1</span>
+            </div>
+        </div>
     );
 }
 
