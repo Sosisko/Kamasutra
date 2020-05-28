e455328c009d3454523759813ebaf84518ed3bac
Sosisko <54500448+Sosisko@users.noreply.github.com>
2020-05-28 23:28:30 +0500

    Add files via upload

--- src/Components/Profile/MyPosts/MyPosts.jsx
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -5,10 +5,10 @@ import Post from "./Post/Post";
 const MyPosts = () => {
     return (
         <div>My posts
-           <div>New post</div>
-           <Post message = 'Привет, как дела ?' likesCount='0'/>
-           <Post message = 'У меня всё ок!' likesCount='23'/>
-
+            <div>New post</div>
+            <Post message='Привет, как дела ?' likesCount='0'/>
+            <Post message='У меня всё ок!' likesCount='23'/>
+            <Post message='А у тебя сколько лайков ?' likesCount='1488'/>
         </div>
 
     );
